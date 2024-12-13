9bce4e1b373376c326bd146dd80083485266dd5e
danbordakov <danbordakov@yandex.ru>
2024-12-13 12:19:28 +0300

    test resize

--- scripts/viewportHeight.js
+++ scripts/viewportHeight.js
@@ -3,4 +3,8 @@ window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.getElementById('testtest').textContent = vh * 100;
 });
+
+// document.getElementById('testtest').textContent = vh;
+// insert.innerHTML = 'wow' + vh;
