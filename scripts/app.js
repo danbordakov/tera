10b92c511488bb7e39b8cb1b5552aa90980b1b3c
danbordakov <danbordakov@yandex.ru>
2025-01-03 14:03:03 +0300

    test

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
+let buttons = [...document.querySelectorAll('.navButton')];
+console.log(buttons);
+
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
+    // console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');;
+      entry.target.classList.add('show');
     } else {
       entry.target.classList.remove('show');
     }
